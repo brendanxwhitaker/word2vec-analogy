91c548d273ea2ce3641a85b033a33128b3e96569
brendanxwhitaker <whitaker.213@osu.edu>
2019-01-03 19:20:44 -0500

    problems

---
+++ word2vec/trunk/accuracy-test.sh
@@ -0,0 +1,2 @@
+./compute-accuracy ~/HIER/pretrained/word2vec-text8-vectors.bin 30000 < questions-words.txt
+# to compute accuracy with the full vocabulary, use: ./compute-accuracy vectors.bin < questions-words.txt
--- word2vec/trunk/demo-word-accuracy.sh
+++ word2vec/trunk/demo-word-accuracy.sh
@@ -4,5 +4,5 @@ if [ ! -e text8 ]; then
   gzip -d text8.gz -f
 fi
 time ./word2vec -train text8 -output vectors.bin -cbow 1 -size 200 -window 8 -negative 25 -hs 0 -sample 1e-4 -threads 20 -binary 1 -iter 15
-./compute-accuracy vectors.bin 30000 < questions-words.txt
+./compute-accuracy ~/HIER/pretrained/word2vec-text8-vectors.bin 30000 < questions-words.txt
 # to compute accuracy with the full vocabulary, use: ./compute-accuracy vectors.bin < questions-words.txt
